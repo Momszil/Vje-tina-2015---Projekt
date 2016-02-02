9ca5ca32827c3db72fe1890dd4fe375785c03ace
Vedran Momčilović <vmomszil@gmail.com>
2016-02-02 23:42:38 +0100

    Buđenje iz mrtvih

--- Generator Kratica/Generator Kratica/Controllers/HomeController.cs
+++ Generator Kratica/Generator Kratica/Controllers/HomeController.cs
@@ -4,19 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Generator_Kratica.Models.Trie;
+using Generator_Kratica.Models;
 
 namespace Generator_Kratica.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         private Trie<string> trie = new Trie<string>();
 
+        public ActionResult Index()
+        {
+            
+            trie.Put("Marko", "Marko");
+            trie.Put("Ivan", "Ivan");
+            // TODO NATRPAVANJE U TRIE METODU
+            return View(new GenViewModel());
+        }
         
+        [HttpPost]
+        public ActionResult Index(GenViewModel model)
+        {
+            model.results = new List<string>();
+            model.results.Add("PERO");
+            model.results.Add("vedran");
+            //TODO NATRPAVANJE REZULTATA
+            return PartialView("Generate", model);
+        }
+
+        [HttpPost]
+        public ActionResult GoBack()
+        {
+            return Redirect("/");
+        }
 
         /*
         public ActionResult About()
---
+++ Generator Kratica/Generator Kratica/Models/GenViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Kratica.Models
+{
+    public class GenViewModel
+    {
+        public string request { get; set; }
+
+        public List<string> results { get; set; }
+    }
+}
